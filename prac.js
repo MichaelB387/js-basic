0836718a9cc0bad09a79383d74ca447c85b86317
MichaelB387 <30610205+MichaelB387@users.noreply.github.com>
2018-03-06 23:45:15 +1100

    more examples

--- prac.js
+++ prac.js
@@ -1,7 +1,29 @@
 // Variable
-var a = 4
+var a = 4;
 // Variable
-var b = 5
+var b = 5;
 // Variable
-var sum = a + b
-console.log(sum)
+var c = 2;
+var sum = a + b;
+// Variable is empty but not undefined
+var emptyInside = null;
+// Follows arithmatic rules
+var result = a + b * c;
+// shorthand version will add 4 to variable a
+a += 4;
+//Unary Operator will increase by value of 1
+b++;
+// conditional statement
+
+if (a > b) {
+  isItBigger = console.log("a is bigger then b");
+} else {
+  isItBigger = console.log("b is biger then a");
+}
+
+console.log(isItBigger);
+
+console.log(sum);
+console.log(result);
+console.log(a);
+console.log(b);
