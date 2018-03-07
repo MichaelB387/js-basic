a71e8b186824b4da5a1e67f6cb555f8ecf4b382c
MichaelB387 <30610205+MichaelB387@users.noreply.github.com>
2018-03-07 12:38:27 +1100

    function example

--- prac.js
+++ prac.js
@@ -23,22 +23,38 @@ if (a > b) {
 console.log(isItBigger);
 
 // Ternary operator
-a === b ? console.log("Match") : console.log("No Match")
-
+a === b ? console.log("Match") : console.log("No Match");
 
 //Array
-    //index   0     1       2
-var pens = ["red","blue","green"]
+//index   0     1       2
+var pens = ["red", "blue", "green"];
 //will output blue
-console.log(pens[1])
+console.log(pens[1]);
 //will output array length, the length is a property of an array
-console.log("Array Length:", pens.length)
+console.log("Array Length:", pens.length);
 //using a method on the array
-pens.reverse()
+pens.reverse();
 //will output the array in reverse order
-console.log(pens)
+console.log(pens);
 
 console.log(sum);
 console.log(result);
 console.log(a);
 console.log(b);
+
+function findBiggestFraction(a, b) {
+    var result
+  a > b ? result = ["firstFraction ", a] : result = ["secondFraction ", b]
+  return result
+}
+
+var firstFraction = 3 / 4;
+var secondFraction = 5 / 7;
+
+var fractionResult = findBiggestFraction(firstFraction, secondFraction);
+
+console.log("First fraction result: ", firstFraction)
+console.log("Second fraction result: ", secondFraction)
+console.log("Fraction " + fractionResult[0] + " with a value of " + fractionResult[1] + " is the biggest!")
+
+
